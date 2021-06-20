9560b2956c967aaec7758733ab01c4766780c2b1
Tombra <tombra4ril@gmail.com>
2021-06-20 22:03:09 +0100

    Allow input to the action main.js file

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,11 @@
-console.log("My first hello world action")
+const core = require("@actions/core")
+
+const first_greeting = core.getInput("first-greeting")
+const second_greeting = core.getInput("second-greeting")
+const third_greeting = core.getInput("third-greeting")
+
+console.log(`Hello ${first_greeting}, how are you doing!`)
+console.log(`Hello ${second_greeting}, how are you doing, was just greeting ${first_greeting}!`)
+if(third_greeting){
+  console.log(`Hello ${third_greeting}, how are you doing, I just greeted ${first_greeting} and ${second_greeting}!`)
+}
